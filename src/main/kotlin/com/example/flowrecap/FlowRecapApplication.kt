062362a6c08117453eab6b844f36afa507c61da1
fredrik <fredrik.a.holtet@gmail.com>
2022-10-11 09:46:49 +0200

    commit to git to enable security and GithubActions

---
+++ src/main/kotlin/com/example/flowrecap/FlowRecapApplication.kt
@@ -0,0 +1,12 @@
+package com.example.flowrecap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowRecapApplication
+
+fun main(args: Array<String>)  {
+
+    runApplication<FlowRecapApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/flowrecap/FlowRecapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flowrecap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowRecapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
