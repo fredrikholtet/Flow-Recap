27ecb5d209044c1821610db164cfb184abcef128
fredrikholtet <fredrik.a.holtet@gmail.com>
2022-10-11 09:12:22 +0000

    fixed package in controller

--- src/main/kotlin/com/example/flowrecap/testController/Controller.kt
+++ src/main/kotlin/com/example/flowrecap/testController/Controller.kt
@@ -1,4 +1,4 @@
-package testController
+package testController com.example.flowrecap.testController
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
