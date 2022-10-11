2cba18ff2c9a90e4f5662ae8c973ceb307b252a4
fredrik <fredrik.a.holtet@gmail.com>
2022-10-11 10:59:25 +0200

    Hello World

---
+++ src/main/kotlin/testController/Controller.kt
@@ -0,0 +1,12 @@
+package testController
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @GetMapping("/")
+    fun hei() = "Halla"
+
+}
